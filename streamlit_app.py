69a888592cc948d84a2cb0a83d15649ea136e678
Paulo <p.o.marttins@gmail.com>
2021-12-30 23:39:21 -0300

    New resources

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,9 @@ Here's our first attempt at using data to create a table:
 import streamlit as st
 import pandas as pd
 df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
+  'coluna 1': [1, 2, 3, 4],
+  'Coluna 2': [10, 20, 30, 40]
 })
 
+print('Olá, essa é a contrução!')
 df
