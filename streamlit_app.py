2e104c665788a7315d110e3e44f4c164f270dcf6
Paulo <p.o.marttins@gmail.com>
2021-12-30 23:36:46 -0300

    New resources

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,17 @@
 # Libera Dados @pmrtys 2021
 
-print('Olá, essa é a contrução!')
+#print('Olá, essa é a contrução!')
+
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+import streamlit as st
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
